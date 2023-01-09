50983622291dec87518a5a92d7d0851bf9a63b1b
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2023-01-09 16:20:00 +0100

    Add Update Add Update Add Update Add Update Add Update

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,15 +15,15 @@ function Home() {
         <div className="container">
           <div className="text-wrapper">
             <h1 data-cy="homePageTitle" className="title">
-              Halo, saya Lutfhi Ramadhan
+              Hello, Felipe Palacio
             </h1>
             <p className="description">
-              Saya seorang pekerja lepas pengembang web, fotograper, desainer
+              Felipe Palacio pekerja lepas pengembang web, fotograper, desainer
               antarmuka pengguna, dan penulis.
             </p>
 
             <Link href="/contact" className="cta">
-              Kontak Saya
+              Kontak Felipe
             </Link>
           </div>
 
